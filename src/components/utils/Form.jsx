d8e1a35c5ef011d2ec968a8699d1859121d406c3
devisrael <devisrael560@gmail.com>
2025-01-08 16:54:23 -0300

    fix: error name

--- src/components/utils/Form.jsx
+++ src/components/utils/Form.jsx
@@ -30,7 +30,7 @@ export function Form({ actionForm, state, handleChange, feedback }) {
                             className: "before:content-none after:content-none",
                         }}
                     />
-                    {state.errors.name && <p className="text-red-500">{state.errors.email}</p>}
+                    {state.errors.name && <p className="text-red-500">{state.errors.name}</p>}
                     <Typography variant="h6" className="text-black dark:text-white -mb-3">
                         Seu Email
                     </Typography>
