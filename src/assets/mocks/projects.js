c68f4a45503ef38d4f76711ef11112156c0afb20
devisrael <devisrael560@gmail.com>
2025-03-10 10:05:08 -0300

    fix: JS word

--- src/assets/mocks/projects.js
+++ src/assets/mocks/projects.js
@@ -47,7 +47,7 @@ export const projects = [
         "desc": "ClosedFun é uma plataforma interativa desenvolvida para proporcionar uma experiência imersiva em entretenimento digital. O projeto combina tecnologia moderna com uma interface intuitiva, permitindo aos usuários explorarem conteúdos exclusivos, participarem de eventos e interagirem de forma dinâmica. Construído com NextJS, TypeScript e TailwindCSS, o ClosedFun prioriza performance, usabilidade e escalabilidade. Durante o desenvolvimento, foram aplicadas técnicas avançadas de otimização e integração para garantir uma experiência fluida e responsiva. Este projeto reforça minha expertise em desenvolvimento web, criação de interfaces interativas e implementação de soluções escaláveis.",
         icons: [
             {
-                title: 'Next',
+                title: 'NextJS',
                 icon: RiNextjsFill
             },
             {
